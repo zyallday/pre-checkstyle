6fc5e42632cd71fc5bf51faef61b6c1929fdfba5
Desert <zyallday@gmail.com>
2016-05-25 08:19:51 +0800

    init pre-checkstyle
    Signed-off-by: Desert <zyallday@gmail.com>

---
+++ app/src/main/java/com/example/desert/checkstyle/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.desert.checkstyle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  public static final int H = 0;
+
+          @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+  }
+}
